75f9fdfa6863f725a49cea506696db1475cadd12
PaoloRusso3196 <73228011+PaoloRusso3196@users.noreply.github.com>
2021-11-05 12:15:29 +0100

    Delete Biflussi.sh

--- PcapManagment/Biflussi.sh
+++
@@ -1,8 +0,0 @@
-#I risultati vengono inseriti nella cartella 'data/Cattura_*/Analisi' per ogni cattura
-
-Catture=$(find data/* -maxdepth 0 -type d)
-for catt in $Catture; do #per ogni cartella di cattura
-	mkdir $catt/Analisi
-	tshark -r $catt/*.pcap* -V -o "ssl.debug_file:$catt/Analisi/ssldebug.log" -o "ssl.desegment_ssl_records: TRUE" -o "ssl.desegment_ssl_application_data: TRUE" -o "ssl.keys_list: C:\Users\Utente\.script_bash\sslkeylogfile.txt" > $catt/Analisi/TlsFiltrato.pcap
-done
-
