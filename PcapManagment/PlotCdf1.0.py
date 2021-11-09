32f620f9fb257f4f9ccb98e31d1c33c245dd9cff
PaoloRusso3196 <73228011+PaoloRusso3196@users.noreply.github.com>
2021-11-09 11:44:13 +0100

    Add files via upload

--- PcapManagment/PlotCdf1.0.py
+++ PcapManagment/PlotCdf1.0.py
@@ -11,7 +11,6 @@ list_IatPkt=[]
 list_IatMsg=[]
 list_PktSize=[]
 list_MsgSize=[]
-list_App=[]
 TypeList=["App_Name","Activty"]
 cattura=1
 "----------------------------------------------------------------------"
@@ -29,11 +28,7 @@ def PloatCdf(list_element):
     plt.show()
     
     
-"-----------------------------------------------------------------------"
-
-    
-                                      
-                             
+#1)CASO IN CUI VADO A CONSIDERARE IL DATAFRAME PER APP O PER ATTIVITA'
 "-----------------------------------------------------------------------"
 df = pd.read_csv(path, delimiter=';')
 df1= pd.read_csv(path, delimiter=';')
@@ -55,18 +50,56 @@ for x in TypeList:
                           list_IatMsg=ast.literal_eval((iatmsg_csv))
                           msgsz_csv=str(df["SIZE_MESSAGE"][i])
                           list_MsgSize=ast.literal_eval((msgsz_csv))
-                          
-       
-                          
-        
-                  #print(list_IatPkt)
-                  PloatCdf(list_IatPkt)
-                  PloatCdf(list_PktSize)
-                  PloatCdf(list_IatMsg)
-                  PloatCdf(list_MsgSize)
+                 #print(list_IatPkt)
+                  #PloatCdf(list_IatPkt)
+                  #PloatCdf(list_PktSize)
+                  #PloatCdf(list_IatMsg)
+                  #PloatCdf(list_MsgSize)
                   list_IatPkt.clear()
                   list_PktSize.clear()
                   list_IatMsg.clear()
                   list_MsgSize.clear()
-                
+"..........................................................................................."                  
+#2)CASO IN CUI VADO A CONSIDERARE IL DATAFRAME PRIMA PER APP E POI PER L' ATTIVITA' ASSOCIATA
 "--------------------------------------------------------------------------------"
+app=df1.drop_duplicates(subset =TypeList[0])
+#print(app)
+for app_i in app.index:
+    appName=str(df1[TypeList[0]][app_i])
+    df_capture=df[TypeList[0]]==appName
+    filtered_df_capture = df[df_capture]
+    #print(filtered_df_capture[TypeList[1]])
+    activity=filtered_df_capture.drop_duplicates(subset =TypeList[1])
+    #print(activity[TypeList[1]])
+    for activity_i in activity.index:
+          
+          Activity=str(activity[TypeList[1]][activity_i])
+          df_capAct=filtered_df_capture[TypeList[1]]==Activity
+          filtered_AppAct = filtered_df_capture[df_capAct]
+          
+          print(filtered_AppAct)
+          if len(filtered_AppAct)>0:
+                    for i in filtered_AppAct.index:
+                            iatpkt_csv=str(df["ARRIVAL_TIME_PACKET"][i])
+                            list_IatPkt=ast.literal_eval((iatpkt_csv))
+                            pktsz_csv=str(df["PACKET_SIZE"][i])
+                            list_PktSize=ast.literal_eval((pktsz_csv))
+                            iatmsg_csv=str(df["ARRIVAL_TIME_MSG"][i])
+                            list_IatMsg=ast.literal_eval((iatmsg_csv))
+                            msgsz_csv=str(df["SIZE_MESSAGE"][i])
+                            list_MsgSize=ast.literal_eval((msgsz_csv))
+                    #print(list_IatPkt)
+                    #PloatCdf(list_IatPkt)
+                    #PloatCdf(list_PktSize)
+                    #PloatCdf(list_IatMsg)
+                    #PloatCdf(list_MsgSize)
+                    list_IatPkt.clear()
+                    list_PktSize.clear()
+                    list_IatMsg.clear()
+                    list_MsgSize.clear()
+ 
+        
+    
+    
+         
+         
