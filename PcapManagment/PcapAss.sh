451821b3a03d75e6fa811bd3a2a0fc417d475ad9
PaoloRusso3196 <73228011+PaoloRusso3196@users.noreply.github.com>
2021-12-01 22:49:35 +0100

    Add files via upload

--- PcapManagment/PcapAss.sh
+++ PcapManagment/PcapAss.sh
@@ -4,13 +4,10 @@ Catture=$(find data/* -maxdepth 0 -type d)
 for catt in $Catture; do #per ogni cartella di cattura
 	mkdir $catt/Analisi
         percorso=$catt/*.pcap*
-     
-       	
         SUBSTRING=$(echo $percorso| cut -d'/' -f 3)
-    
         NAME=$(echo $SUBSTRING| cut -d'.' -f 1)
         echo $NAME
         NameAss="${NAME}Ass"
         echo $NameAss
-	tshark -r $catt/*.pcap* -V -o "ssl.debug_file:$catt/Analisi/ssldebug.log" -o "ssl.desegment_ssl_records: TRUE" -o "tls.desegment_ssl_records: TRUE" -o "tls.desegment_ssl_application_data: TRUE" -o "tls.keys_list:C:/Users/Utente/.script_bash/sslkeylogfile.txt" -F pcap -w $catt/Analisi/$NameAss.pcap
+	tshark -r $catt/*.pcap* -V -o "tls.debug_file:$catt/Analisi/ssldebug.log" -o "tls.desegment_ssl_records: TRUE" -o "tls.desegment_ssl_application_data: TRUE" -o "tls.keys_list:C:/Users/Utente/.script_bash/sslkeylogfile.txt" -F pcap -w $catt/Analisi/$NameAss.pcap
 done
